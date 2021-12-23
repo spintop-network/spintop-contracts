8ab44c334b00f1c9187dcbb42c9ce0eb7d3de8b1
T490s <batudal@gmail.com>
2021-12-24 01:32:55 +0300

    fantom test deploy

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+  const SpinMock = await ethers.getContractFactory("SpinMock");
+  const spinMock = await SpinMock.deploy();
+  await spinMock.deployed();
+  const mockAdress = spinMock.address;
+  console.log("SpinMock deployed: ", mockAdress);
+
+  const SpinStakable = await ethers.getContractFactory("SpinStakable");
+  const spinStakable = await SpinStakable.deploy(mockAdress, mockAdress);
+  await spinStakable.deployed();
+  console.log("SpinStakable deployed: ", spinStakable.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
