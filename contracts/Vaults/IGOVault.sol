cac78f3d134b94f3faa5bbe7a78616f0b16482ef
T490s <batudal@gmail.com>
2022-03-12 12:04:00 +0300

    IGOVault & ISpinStakable

--- contracts/Vaults/IGOVault.sol
+++ contracts/Vaults/IGOVault.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "../Interfaces/ISpinStakable.sol";
 
 
 contract IGOVault is ERC20 {
@@ -24,6 +25,7 @@ contract IGOVault is ERC20 {
         uint256 totalAmount;
     }
     VaultInfo public vaultInfo;
+
 
     constructor (
         string memory _gameName, 
@@ -50,11 +52,27 @@ contract IGOVault is ERC20 {
 
     // function calculatePairPrice () private {}
 
-    function deposit(uint _amount) public nonReentrant {
-        strategy.beforeDeposit();
+    function balance() public view returns (uint) {
+        return IERC20(vaultInfo.spinToken).balanceOf(address(this));
+    }
+
+    function earn() public {
+        IERC20(vaultInfo.spinToken).safeTransfer(address(this), balance());
+        deposit();
+    }
+
+    function deposit(uint _amount) public { // add reentrancy
+        uint256 _bal = balance();
+        if (_bal > 0) {
+            ISpinStakable().deposit(_bal);
+        }
+
+        // deposit into farm 0
+        // add liquidity 
+        // deposit into farm all
 
         uint256 _pool = balance();
-        want().safeTransferFrom(msg.sender, address(this), _amount);
+        IERC20(vaultInfo.spinToken).safeTransferFrom(msg.sender, address(this), _amount);
         earn();
         uint256 _after = balance();
         _amount = _after.sub(_pool); // Additional check for deflationary tokens
@@ -65,5 +83,21 @@ contract IGOVault is ERC20 {
             shares = (_amount.mul(totalSupply())).div(_pool);
         }
         _mint(msg.sender, shares);
+    }
+
+    function addLiquidity() internal {
+        uint256 outputHalf = IERC20(output).balanceOf(address(this)).div(2);
+
+        if (lpToken0 != output) {
+            IUniswapRouterETH(unirouter).swapExactTokensForTokens(outputHalf, 0, outputToLp0Route, address(this), block.timestamp);
+        }
+
+        if (lpToken1 != output) {
+            IUniswapRouterETH(unirouter).swapExactTokensForTokens(outputHalf, 0, outputToLp1Route, address(this), block.timestamp);
+        }
+
+        uint256 lp0Bal = IERC20(lpToken0).balanceOf(address(this));
+        uint256 lp1Bal = IERC20(lpToken1).balanceOf(address(this));
+        IUniswapRouterETH(unirouter).addLiquidity(lpToken0, lpToken1, lp0Bal, lp1Bal, 1, 1, address(this), block.timestamp);
     }
 }
